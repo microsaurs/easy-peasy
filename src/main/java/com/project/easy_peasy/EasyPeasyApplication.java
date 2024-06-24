4ab034395b02bf4bea760f82feaf68993f9352ec
microsaurs <microsaurs@gmail.com>
2024-06-24 20:08:34 +0900

    first commit

---
+++ src/main/java/com/project/easy_peasy/EasyPeasyApplication.java
@@ -0,0 +1,13 @@
+package com.project.easy_peasy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyPeasyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyPeasyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/easy_peasy/EasyPeasyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.easy_peasy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyPeasyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
