3e4f05e97c8235abf58157196ad4d10df932914c
microsaurs <microsaurs@gmail.com>
2024-07-01 19:36:59 +0900

    test

---
+++ src/main/java/com/project/easy_peasy/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.project.easy_peasy.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/api/test")
+    public String hello() {
+        return "테스트";
+    }
+}
