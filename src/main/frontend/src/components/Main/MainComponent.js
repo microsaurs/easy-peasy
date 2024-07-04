136499a8a6f516db12e3dbbb9aa0c360ef12c065
fhwang98 <fhwang9815@gmail.com>
2024-07-04 22:19:49 +0900

    mainpage css 수정

--- src/main/frontend/src/components/Main/MainComponent.js
+++ src/main/frontend/src/components/Main/MainComponent.js
@@ -1,57 +1,59 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import "../../assets/css/Main.css";
+
 import logo from "../../assets/images/logo.png";
+import step1 from '../../assets/images/step1.JPG';
+import step2 from '../../assets/images/step2.JPG';
+import step3 from '../../assets/images/step3.JPG';
 
-const Title = () => {
-  return (
-    <div id="title">
-      <img id="logo" src={logo} />
-      <div>이지피지</div>
-      <div>Easy Peasy</div>
-    </div>
-  );
-};
+// Title 컴포넌트
+const Title = () => (
+  <div id="title">
+    <img id="logo" src={logo} alt="Logo" />
+    <div>이지피지</div>
+    <div>Easy Peasy</div>
+  </div>
+);
 
+// Step 데이터
 const stepsData = [
   {
     step: "STEP 1",
     description: "상세기획이 필요한 \n 이미지/화면안을 \n 가져온다",
+    image: step1
   },
   {
     step: "STEP 2",
     description: "영역 정의서가 필요한 \n 영역을 드래그하고, \n 쉽게 넘버링한다",
+    image: step2
   },
   {
     step: "STEP 3",
     description: "벌써 완료! \n 이미지를 저장하고 \n 활용한다!",
+    image: step3
   },
 ];
 
-const Steps = () => {
-  return (
-    <div className="steps">
-      {stepsData.map(({ step, description }, index) => (
-        <div key={index} className="step">
-          <div className="step-title">{step}</div>
-          <img
-            src={require(`../../assets/images/step${index + 1}.JPG`)}
-            className="step-image"
-          />
-          <div className="step-description">
-            {description.split('\n').map((line, i) => (
-              <React.Fragment key={i}>
-                {line}
-                <br />
-              </React.Fragment>
-            ))}
-          </div>
+// Steps 컴포넌트
+const Steps = () => (
+  <div id="steps">
+    {stepsData.map(({ step, description, image }, index) => (
+      <div key={index} className="step">
+        <div className="step-title">{step}</div>
+        <img
+          src={image}
+          className="step-img"
+        />
+        <div className="step-desc">
+          {description}
         </div>
-      ))}
-    </div>
-  );
-};
+      </div>
+    ))}
+  </div>
+);
 
+// MainComponent 컴포넌트
 const MainComponent = () => {
   const navigate = useNavigate();
 
@@ -66,7 +68,7 @@ const MainComponent = () => {
         서비스기획자/PM을 위한 <span>세상 쉬운</span> 상세계획서 작성 툴
       </div>
       <div id="btn-box">
-        <button onClick={goToStart}>
+        <button id="start-btn" onClick={goToStart}>
           시작하기
         </button>
       </div>
